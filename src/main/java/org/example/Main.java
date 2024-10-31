6c5e3c723c00269212904dec730929ec967cba75
RicardoAugustoWegner <ricardoaugustowegner@gmail.com>
2024-10-31 18:09:04 -0300

    first commit

---
+++ src/main/java/org/example/Main.java
@@ -0,0 +1,13 @@
+package org.example;
+
+public class Main {
+    public static void main(String[] args) {
+
+        System.out.println("Hello World");
+
+        System.out.println("Gerência de Configuração de Software 2024");
+
+        int resultado = 3 + 7;
+        System.out.println("O resultado de 3 + 7 é: " + resultado);
+    }
+}
